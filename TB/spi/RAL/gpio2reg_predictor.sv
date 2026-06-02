ded8ee32f227fcf6547d1efafdda17e5a240b39c
Vladimir Beran <beranvl5@seznam.cz>
2026-06-02 23:25:54 +0200

    Predictor updated (input en consider), test stimuli added.

--- TB/spi/RAL/gpio2reg_predictor.sv
+++ TB/spi/RAL/gpio2reg_predictor.sv
@@ -5,6 +5,7 @@ class gpio2reg_predictor extends uvm_component;//uvm_subscriber #(gpio_seq_item)
   virtual gpio_if gpio_vif;
   spi_reg_block ral;
   //uvm_reg_data_t value;
+  bit [7:0] pred_value;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -43,12 +44,14 @@ class gpio2reg_predictor extends uvm_component;//uvm_subscriber #(gpio_seq_item)
     forever begin
       @(gpio_vif.gpio_in);
 
+      pred_value = gpio_vif.gpio_in & ral.input_en.get_mirrored_value();
+
       void'(ral.gpio_in.bits.predict(
-        .value(gpio_vif.gpio_in),
+        .value(pred_value),
         .kind(UVM_PREDICT_DIRECT),
         .path(UVM_BACKDOOR)
       ));
-      `uvm_info(get_type_name(), $sformatf("Predicted GPIO_STATUS.gpio_value = 0x%0h", gpio_vif.gpio_in), UVM_HIGH)
+      `uvm_info(get_type_name(), $sformatf("Predicted GPIO_STATUS.gpio_value = 0x%0h", pred_value), UVM_HIGH)
     end
   endtask
 endclass
--- TB/tests/gpio_in_test.sv
+++ TB/tests/gpio_in_test.sv
@@ -27,6 +27,19 @@ class gpio_in_test extends base_test;
     m_clk_rst_seq = clk_rst_sequence::type_id::create("clk_rst_seq");
     m_clk_rst_seq.start(m_clk_rst_env.agent.sequencer);
 
+    // try before input enable is set
+    repeat(3) begin
+      #(100ns);
+      m_gpio_seq = gpio_sequence::type_id::create("gpio_seq");
+      m_gpio_seq.start(m_gpio_env.m_agent.m_sequencer);
+      #(5us);
+
+      m_spi_env.m_regmodel.gpio_in.mirror(status, UVM_CHECK);
+      data = m_spi_env.m_regmodel.gpio_in.get_mirrored_value();
+      `uvm_info(get_type_name(), $sformatf("gpio in : %h", data), UVM_LOW)
+
+    end
+
     // input enable
     #(100ns);
     m_spi_env.m_regmodel.input_en.write(status, 8'hff);
